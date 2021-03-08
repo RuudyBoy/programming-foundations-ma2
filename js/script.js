cd41de185ca0a5a3dc32131088d001126f7d61b8
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-03-08 16:14:55 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//*Question 1*//
+
+var pets= [
+    {
+        type:"cat",
+        age:5.5,
+    },
+    {
+        type:"dog",
+        age:3.8,
+    },
+    {
+        type:"parrot",
+        age:4.0,
+    },
+];
+
+
