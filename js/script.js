d9dd3146b57bae31cf77c1f1239983d3ab860923
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-03-14 17:31:45 +0100

    k

--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
+//* Question 1 *//
+
+var pets = [
+	{
+		type: "cat",
+		age: 5.5,
+	},
+	{
+		type: "dog",
+		age: 3.8,
+	},
+	{
+		type: "parrot",
+		age: 4.0,
+	},
+];
+
+for (var i = 0; i < pets.length; i++) {
+    
+     var age =pets[i].age;
+     var type=pets[i].type;
+
+     if (age >=4) {
+         console.log(type);
+     }
+     
+}
+
+
+//*Question 2*//
+
+function logBoolean(booleanValue) {
+    var typeofFirst = typeof booleanValue;
+
+    if (typeofFirst !== true) {
+
+        return "Please pass a boolean value in";
+    }
+
+    return booleanValue;
+}
+
+var result = logBoolean;
+console.log(result);
+
+
+
 //*Question 3*//
 var heading = document.querySelector("h2")
 var button = document.querySelector("button")
@@ -10,10 +57,7 @@ function changeHeading () {
 
 button.onclick = changeHeading;
 
-//*Question 2*//
 
-function logBolean(boleanValue) {
-    console.log(boleanValue);
-}
 
-logBolean (true);
+
+
